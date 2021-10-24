ef828718ecdb8d3e535d79262f76b6b8f0ea833b
Dominik <dominik.geissler@rwth-aachen.de>
2021-10-24 20:05:08 +0200

    Upload

--- bootstrapdjango/urls.py
+++ bootstrapdjango/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.conf.urls.static import static
-from . import settings
+from django.conf import settings
 
 urlpatterns = [
     path('', include('upload_eventlog.urls'))
 ]
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- upload_eventlog/views.py
+++ upload_eventlog/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 def upload_page(request):
-    return render(request, 'upload_page.html', {'message': 'Hallo'})
+    # if user wants to upload a file
+    if request.method == 'POST' and request.FILES['upload_file']:
+        # get file from form
+        file = request.FILES['upload_file']
+        # create new FileSystemStorage
+        fs = FileSystemStorage()
+        # save file and get filename
+        filename = fs.save(file.name, file)
+        # get file url
+        file_url = fs.url(filename)
+        # return file url
+        return render(request, 'upload_page.html', {'file_url': file_url})
+    # return empty page
+    return render(request, 'upload_page.html')
