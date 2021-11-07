b27db9896857c5b2f8d19356e44ccb9f2a86effb
Dominik Geißler <dominik.geissler@rwth-aachen.de>
2021-11-07 14:58:37 +0100

    Model für Logs hinzugefügt, Admin-Seite included

--- bootstrapdjango/urls.py
+++ bootstrapdjango/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.contrib import admin
 from django.conf import settings
 
 urlpatterns = [
@@ -26,3 +27,4 @@ urlpatterns = [
 # if debug mode is enabled, also load media as static page
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += path('admin/', admin.site.urls)
