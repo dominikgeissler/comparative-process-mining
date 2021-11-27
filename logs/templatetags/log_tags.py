855c1732096b587bda28d8eaea5306e61eb547a9
Dominik Geißler <dominik.geissler@rwth-aachen.de>
2021-11-27 17:10:22 +0100

    correction for comparison filter

--- logs/templatetags/log_tags.py
+++ logs/templatetags/log_tags.py
@@ -6,14 +6,12 @@ from helpers.dfg_helper import convert_dfg_to_dict
 
 register = template.Library()
 
-
 @register.filter
-def get_metrics(log):
-    log1 = Log.objects.get(pk=1)
-    if ComparisonMetrics(log.pm4py_log(), log1.pm4py_log()).check_log_equality():
-        return LogMetrics(log.pm4py_log()).get_metrics()
+def get_metrics(log, initial):
+    if initial == log:
+        return LogMetrics(initial.pm4py_log()).get_metrics()
     else:
-        return ComparisonMetrics(log.pm4py_log(), log1.pm4py_log()).get_comparison()
+        return ComparisonMetrics(initial, log).get_comparison()
 
 
 @register.filter
