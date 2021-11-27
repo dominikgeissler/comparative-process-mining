98cc56a15a229007a83b06d4e46d2aa288089f0f
PatrickF227 <92594943+PatrickF227@users.noreply.github.com>
2021-11-27 11:16:58 +0100

    highlight_nonstandard_activities (log_tags.py)

--- logs/templatetags/log_tags.py
+++ logs/templatetags/log_tags.py
@@ -14,6 +14,7 @@ def get_metrics(log):
 @register.filter
 def get_graph(log):
     return json.dumps(
-        dfg_dict_to_g6(
-            convert_dfg_to_dict(
-                LogObjectHandler(log).generate_dfg())))
+        highlight_nonstandard_activities(
+            dfg_dict_to_g6(
+                convert_dfg_to_dict(
+                    LogObjectHandler(log).generate_dfg()))))
