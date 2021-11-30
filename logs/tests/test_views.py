9e9c7abcf893deec2f3bcb83bfd3fe194eaeae6a
Dominik Geißler <dominik.geissler@rwth-aachen.de>
2021-11-30 12:53:19 +0100

    tests

--- logs/tests/test_views.py
+++ logs/tests/test_views.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 class AboutViewTests(TestCase):
-    def test_get(self):
+    def test_get_about(self):
         response = self.client.get(reverse("about_view"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "This is the about page.")
