146b2b96cbb2391ccab173b55688a4b973f4c570
Dominik Geißler <dominik.geissler@rwth-aachen.de>
2021-12-01 15:54:59 +0100

    metric key resolver

--- logs/templatetags/metrics_tags.py
+++ logs/templatetags/metrics_tags.py
@@ -7,3 +7,21 @@ register = template.Library()
 @register.filter
 def is_list(obj):
     return isinstance(obj, List)
+
+@register.filter
+def key_resolve(key):
+    if key == "no_cases":
+        ret = "Number of Cases"
+    elif key == "no_events":
+        ret = "Number of Events"
+    elif key == "no_variants":
+        ret = "Number of Variants"
+    elif key == "avg_case_duration":
+        ret = "Average Case Duration"
+    elif key == "median_case_duration":
+        ret = "Median Case Duration"
+    elif key == "total_case_duration":
+        ret = "Total Case Duration"
+    else:
+        ret = key
+    return ret
