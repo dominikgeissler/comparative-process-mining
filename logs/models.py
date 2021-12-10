04cdcdfe7fe43f64ad36dab4be03a998acd93744
PatrickF227 <92594943+PatrickF227@users.noreply.github.com>
2021-12-10 11:10:58 +0100

    Similarity index update (models.py)

--- logs/models.py
+++ logs/models.py
@@ -122,49 +122,28 @@ class LogObjectHandler(models.Model):
         return sorted(pm4py.get_attribute_values(self.pm4py_log(), "time:timestamp"))
 
     def get_similarity_index(self, reference):
+        """
+        Similarity Index based on the number of common variants or (in other words) common traces
+        """
         if reference and reference.log_object != self.log_object:
-            act_count1 = pm4py.get_attribute_values(
-                self.pm4py_log(), "concept:name")
-            act_count2 = pm4py.get_attribute_values(
-                reference.pm4py_log(), "concept:name")
-            different_activities = 0
-            for key in act_count1:
-                if key in act_count2.keys():
-                    different_activities += abs(
-                        act_count1[key] - act_count2[key])
-                else:
-                    different_activities += act_count1[key]
-            if different_activities > sum(act_count1.values()):
-                different_activities = sum(act_count1.values())
-            res_similar_activities1 = (
-                sum(act_count1.values()) - different_activities)/sum(act_count1.values())
-            """
-            Calculation of the number of similar activities in comparison to the 
-            sum of all activities in act_count1 (Dictionary activities_count).
-            Special feature: 
-            The index emphasis the size of the log.
-            The index is higher if the analyzed log has fewer events compared to the reference log.
-            And the index is lower if the analyzed log has more events than the reference log.
-            """
-            similar_activities = 0
-            for key in act_count1:
-                if key in act_count2.keys():
-                    similar_activities += 1
-            res_similar_activities2 = similar_activities/len(act_count1)
-            """
-            Calculation of the number of similar activities in comparison to the 
-            number of activities in act_count1 (Dictionary activities_count).
-            Special feature: 
-            This index focuses on the number of similar activities (not how often they occur)
-            and neglects the size of or size difference between the two event logs.
-            """
-            res = (res_similar_activities1 + res_similar_activities2)/2
-            return str("%.2f" % round(res*100, 2))+"%"
-            """
-            Result = Similarity index:
-            50% weighting with emphasis on the size or size difference between the logs.
-            And 50% weighting without focusing on the size or size difference of the two logs.
-            """
+            variants_count_log = case_statistics.get_variant_statistics(
+                self.pm4py_log())
+            variants_count_reference = case_statistics.get_variant_statistics(
+                reference.pm4py_log())
+            sum_variants_reference = 0
+            sum_variants_log = 0
+            common_no_variants = 0
+            for x in range(0, len(variants_count_reference)-1):
+                for i in range(0, len(variants_count_log)-1):
+                    # if variants_count_reference[x]['variant'] == variants_count_log[i]['variant']:
+                    if variants_count_reference[x]['variant'] in variants_count_log[i]['variant']:
+                        common_no_variants += min(
+                            variants_count_reference[x]['count'], variants_count_log[i]['count'])
+                sum_variants_reference += variants_count_reference[x]['count']
+            for i in range(0, len(variants_count_log)-1):
+                sum_variants_log += variants_count_log[i]['count']
+            sum = max(sum_variants_reference, sum_variants_log)
+            return str("%.2f" % round(common_no_variants/sum*100, 2))+"%"
         return "100%"
 
     def pm4py_log(self):
