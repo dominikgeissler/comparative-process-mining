e7981236509b9a543f37c3ed7eb1d6fae99e4521
Dominik Geißler <dominik.geissler@rwth-aachen.de>
2021-12-19 08:21:38 +0100

    comment

--- logs/templatetags/filter_tags.py
+++ logs/templatetags/filter_tags.py
@@ -11,8 +11,10 @@ formatstring = "%Y-%m-%d %H:%M:%S"
 def convert_timestamp(timestamp):
     """converts the timestamp to a format django can handle"""
     if type(timestamp) is Timestamp or type(timestamp) is datetime:
+        # timestamp is not formatted yet
         return timestamp.strftime(formatstring)
     else:
+        # timestamp is already formatted
         return timestamp
 
 @register.filter
